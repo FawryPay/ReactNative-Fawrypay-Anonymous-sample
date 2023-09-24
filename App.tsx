212b0bc57e50187c4f9655938005431bc550ed46
Ahmed Kamal <Ahmed.Kamal@Fawry.com>
2023-09-24 09:51:13 +0300

    Updating Pod

--- App.tsx
+++ App.tsx
@@ -1,15 +1,30 @@
-import React, { useEffect } from 'react';
-import { TouchableOpacity, Text, StyleSheet, View, Platform } from 'react-native';
+import React, {useEffect} from 'react';
+import {TouchableOpacity, Text, StyleSheet, View, Platform} from 'react-native';
 import * as Fawry from '@fawry_pay/rn-fawry-pay-sdk';
 import uuid from 'react-native-uuid';
 
-const cartItems : Fawry.BillItems[] = [
-  { itemId: 'item1', description: 'Item 1 Description', quantity: '1', price: '200' },
-  { itemId: 'item2', description: 'Item 2 Description', quantity: '1', price: '20' },
-  { itemId: 'item3', description: 'Item 3 Description', quantity: '1', price: '10' },
+const cartItems: Fawry.BillItems[] = [
+  {
+    itemId: 'item1',
+    description: 'Item 1 Description',
+    quantity: '1',
+    price: '300',
+  },
+  {
+    itemId: 'item2',
+    description: 'Item 2 Description',
+    quantity: '1',
+    price: '200',
+  },
+  {
+    itemId: 'item3',
+    description: 'Item 3 Description',
+    quantity: '1',
+    price: '500',
+  },
 ];
 
-const merchant : Fawry.MerchantInfo = {
+const merchant: Fawry.MerchantInfo = {
   merchantCode:
   Platform.OS === 'android'
     ? '+/IAAY2notgLsdUB9VeTFg==' // Android 
@@ -21,51 +36,85 @@ merchantSecretCode:
   merchantRefNum: uuid.v4().toString(),
 };
 
-const customer : Fawry.CustomerInfo = {
+const customer: Fawry.CustomerInfo = {
   customerName: 'Ahmed Kamal',
   customerMobile: '+1234567890',
   customerEmail: 'ahmed.kamal@example.com',
   customerProfileId: '12345',
 };
 
-const fawryConfig : Fawry.FawryLaunchModel = {
+const fawryConfig: Fawry.FawryLaunchModel = {
   baseUrl: 'https://atfawry.fawrystaging.com/',
   lang: Fawry.FawryLanguages.ENGLISH,
   signature: '',
-  allow3DPayment: false,
+  allow3DPayment: true,
   skipReceipt: false,
   skipLogin: true,
   payWithCardToken: true,
   authCaptureMode: false,
-  allowVoucher: true,
+  allowVoucher: false,
   items: cartItems,
   merchantInfo: merchant,
   customerInfo: customer,
 };
 
 const eventListeners = [
-  { eventName: Fawry.FawryCallbacks.FAWRY_EVENT_PAYMENT_COMPLETED, listener: (data: any) => console.log(Fawry.FawryCallbacks.FAWRY_EVENT_PAYMENT_COMPLETED, data) },
-  { eventName: Fawry.FawryCallbacks.FAWRY_EVENT_ON_SUCCESS, listener: (data: any) => console.log(Fawry.FawryCallbacks.FAWRY_EVENT_ON_SUCCESS, data) },
-  { eventName: Fawry.FawryCallbacks.FAWRY_EVENT_ON_FAIL, listener: (error: any) => console.log(Fawry.FawryCallbacks.FAWRY_EVENT_ON_FAIL, error) },
-  { eventName: Fawry.FawryCallbacks.FAWRY_EVENT_CardManager_FAIL, listener: (error: any) => console.log(Fawry.FawryCallbacks.FAWRY_EVENT_CardManager_FAIL, error) },
+  {
+    eventName: Fawry.FawryCallbacks.FAWRY_EVENT_PAYMENT_COMPLETED,
+    listener: (data: any) =>
+      console.log(Fawry.FawryCallbacks.FAWRY_EVENT_PAYMENT_COMPLETED, data),
+  },
+  {
+    eventName: Fawry.FawryCallbacks.FAWRY_EVENT_ON_SUCCESS,
+    listener: (data: any) =>
+      console.log(Fawry.FawryCallbacks.FAWRY_EVENT_ON_SUCCESS, data),
+  },
+  {
+    eventName: Fawry.FawryCallbacks.FAWRY_EVENT_ON_FAIL,
+    listener: (error: any) =>
+      console.log(Fawry.FawryCallbacks.FAWRY_EVENT_ON_FAIL, error),
+  },
+  {
+    eventName: Fawry.FawryCallbacks.FAWRY_EVENT_CardManager_FAIL,
+    listener: (error: any) =>
+      console.log(Fawry.FawryCallbacks.FAWRY_EVENT_CardManager_FAIL, error),
+  },
 ];
 
-const attachEventListeners = () => eventListeners.forEach(({ eventName, listener }) => Fawry.FawryCallbacks.FawryEmitter.addListener(eventName, listener));
+const attachEventListeners = () =>
+  eventListeners.forEach(({eventName, listener}) =>
+    Fawry.FawryCallbacks.FawryEmitter.addListener(eventName, listener),
+  );
 
-const detachEventListeners = () => eventListeners.forEach(({ eventName }) => Fawry.FawryCallbacks.FawryEmitter.removeAllListeners(eventName));
+const detachEventListeners = () =>
+  eventListeners.forEach(({eventName}) =>
+    Fawry.FawryCallbacks.FawryEmitter.removeAllListeners(eventName),
+  );
 
 export default function App() {
   useEffect(() => {
     attachEventListeners();
-    
+
     return detachEventListeners;
   }, []);
 
   const platformText = Platform.OS === 'android' ? 'Android' : 'iOS';
 
-  const handlePayments = () => Fawry.startPayment(fawryConfig);
+  const handlePayments = () => {
+    merchant.merchantRefNum = uuid.v4().toString();
+    Fawry.startPayment({
+      ...fawryConfig,
+      merchantInfo: {...merchant},
+    });
+  };
 
-  const handleCardsManager = () => Fawry.openCardsManager(fawryConfig.baseUrl, fawryConfig.lang, fawryConfig.merchantInfo, fawryConfig.customerInfo);
+  const handleCardsManager = () =>
+    Fawry.openCardsManager(
+      fawryConfig.baseUrl,
+      fawryConfig.lang,
+      fawryConfig.merchantInfo,
+      fawryConfig.customerInfo,
+    );
 
   return (
     <View style={styles.container}>
@@ -81,8 +130,8 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-  container: { flex: 1, alignItems: 'center', justifyContent: 'center' },
-  platformText: { fontSize: 20, fontWeight: 'bold', marginBottom: 20 },
+  container: {flex: 1, alignItems: 'center', justifyContent: 'center'},
+  platformText: {fontSize: 20, fontWeight: 'bold', marginBottom: 20},
   button: {
     backgroundColor: '#007AFF',
     padding: 10,
@@ -90,10 +139,10 @@ const styles = StyleSheet.create({
     borderRadius: 25,
     marginBottom: 20,
     shadowColor: '#000',
-    shadowOffset: { width: 0, height: 2 },
+    shadowOffset: {width: 0, height: 2},
     shadowOpacity: 0.3,
     shadowRadius: 4,
     elevation: 5,
   },
-  buttonText: { color: 'white', fontSize: 18, fontWeight: 'bold' },
+  buttonText: {color: 'white', fontSize: 18, fontWeight: 'bold'},
 });
